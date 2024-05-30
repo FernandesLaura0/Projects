2e6507954dad55aef7c65bdf963b4712a4dae9ac
FernandesLaura0 <fernandes.laura.2023@gmail.com>
2024-05-30 16:38:23 -0700

    Created a matrix to start the connect4 game

--- Connect4.py
+++ Connect4.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*- 
+import numpy as np
+def create_board():
+    board = np.zeros((6, 7)) #6 rows by 7 columns
+    return board
+
+board = create_board
+pritn(board)
