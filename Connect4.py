b3bfe16b6c81a32eed04d27648f2f3642fb74358
FernandesLaura0 <fernandes.laura.2023@gmail.com>
2024-05-30 16:49:17 -0700

    Player 1 Input

--- Connect4.py
+++ Connect4.py
@@ -5,5 +5,16 @@ def create_board():
     board = np.zeros((6, 7)) #6 rows by 7 columns
     return board
 
-board = create_board
-pritn(board)
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 Input
+    if turn == 0:
+        selection = int(input("Player 1 Make your Selection (0-6): "))
+
+        print(selection)
+
+
+    # Ask for player 2 Input
