2ad727907335e91a1380d2d0440b8be2d7718ecc
FernandesLaura0 <fernandes.laura.2023@gmail.com>
2024-05-30 18:47:24 -0700

    Part 2 done

--- Connect4.py
+++ Connect4.py
@@ -14,7 +14,7 @@ def drop_piece(board, row, col, piece):
     
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
@@ -23,6 +23,32 @@ def get_next_open_row(board, col):
 
 def print_board(board):
     print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return  True
+            
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return  True
+    
+    # Check positively sloped diagonals
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return  True
+
+    # Check for negatively sloped diagonals
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c] == piece and board[r-2][c] == piece and board[r-3][c] == piece:
+                return  True
+
 board = create_board()
 print_board(board)
 game_over = False
@@ -36,6 +62,9 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+        if winning_move(board, 1):
+            print("PLAYER 1 WINS!!! Congrats!!!")
+            game_over = True
 
     # Ask for player 2 Input
     else: 
@@ -44,6 +73,9 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+        if winning_move(board, 2):
+            print("PLAYER 2 WINS!!! Congrats!!!")
+            game_over = True
     
     print_board(board)
     
